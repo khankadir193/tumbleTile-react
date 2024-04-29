cf026cfbaf0959a86237f6800322ff1dbc64dd87
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-29 16:43:43 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='HeaderContainer'>
+        <h1>This is the header content</h1>
+    </div>
+  )
+}
+
+export default Header
