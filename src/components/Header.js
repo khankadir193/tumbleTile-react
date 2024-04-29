562974e1d470114ab1f1f025ceea880682926243
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-29 21:25:31 +0530

    leaderboar-guide-myJumps has been set

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import './Header.css'
+import './Header.css';
+import Image from './Image';
+import guideButton from '../images/Guide/Guide-button.png';
+import myJumps from '../images/Header/myJumps.png';
+import leaderBoardButton from '../images/Leaderboard/leaderboard-button.png';
 
 const Header = () => {
   return (
     <div className='HeaderContainer'>
-        <h1>This is the header content</h1>
+      <div className="topElement-container">
+        <div className="guide-button">
+          <button id="guideButton">
+            <Image src={guideButton} alt="myJumps" />
+          </button>
+        </div>
+        <span>Jumps:xxx</span>
+        <Image src={myJumps} alt="myJumps" />
+        <div className="leaderboardButton">
+          <button id="leaderBoardButton">
+            <img src={leaderBoardButton} alt="myJumps" />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Image.js
@@ -0,0 +1,8 @@
+// Image.js
+import React from 'react';
+
+const Image = ({ src, alt }) => {
+  return <img src={src} alt={alt} />;
+};
+
+export default Image;
