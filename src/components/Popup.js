a72a26d176855b804edfeb99a31ce2a00f8779a8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-30 12:01:44 +0530

    first-display && second display content

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,17 +5,47 @@ import Image from './Image';
 import closeBtn from '../images/Guide/close-button.png';
 import guideBanner from '../images/Guide/Guide-banner.png';
 import character from '../images/Guide/charcter-right.gif';
+import eventGifts from '../images/Guide/event-gifts.png';
+import treasureBox from '../images/Guide/treasure-box.png';
+import warrior from '../images/Guide/warrior.png';
+import display1 from '../images/Guide/display-1.png';
+import gameWorld from '../images/Guide/game-world.png';
+import partyBrust from '../images/Guide/party-brust.png';
+import display2 from '../images/Guide/display-2.png';
+
 
 const Popup = ({ onClose }) => {
   return (
     <div className="popup">
+      <button onClick={onClose}>
+        <Image src={closeBtn} alt="close button" />
+      </button>
+      <div class="guide-character">
+        <Image src={guideBanner} alt="guideBanner" />
+        <Image src={character} alt="characterRight" />
+      </div>
       <div className="popup-content">
-        <button onClick={onClose}>
-          <Image src={closeBtn} alt="close button" />
-        </button>
-        <div class="guide-character">
-          <Image src={guideBanner} alt="guideBanner" />
-          <Image src={character} alt="characterRight" />
+        {/* guide content goes here */}
+        <div class="guide">
+          <div class="event-gifts">
+            <Image src={eventGifts} alt="event-gifts" />
+          </div>
+          <div class="display">
+            <div class="first-display">
+              <div class="treasure-warrior">
+                <Image src={treasureBox} alt="warrior" />
+                <Image src={warrior} alt="warrior" />
+              </div>
+              <Image src={display1} alt="guideBanner" />
+            </div>
+            <div class="second-display">
+              <div class="game-party">
+                <Image src={gameWorld} alt="game-world" />
+                <Image src={partyBrust} alt="partyBrust" />
+              </div>
+              <Image src={display2} alt="display2" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
