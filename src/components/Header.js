829bd3e14d621b03191e5b4834815c763e1c585d
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-05-01 17:28:54 +0530

    left-right character

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,20 @@ import guideButton from '../images/Guide/Guide-button.png';
 import myJumps from '../images/Header/myJumps.png';
 import leaderBoardButton from '../images/Leaderboard/leaderboard-button.png';
 import rewardHistory from '../images/Header/reward-history-icon.gif';
-import titlePosition  from '../images/Header/tile-Position-icon.png';
+import titlePosition from '../images/Header/tile-Position-icon.png';
+import characterLeft from '../images/Header/charcter-left.gif';
+import characterRight from '../images/Header/charcter-right.gif';
 import Popup from './Popup';
 
 const Header = () => {
-  const [showPopup,setShowPopup] = useState();
-  const [popupContent,setPopupContent] = useState();
+  const [showPopup, setShowPopup] = useState();
+  const [popupContent, setPopupContent] = useState();
 
-  const togglePopup = (event)=>{
-    console.log("event...poput...",event);
-    if(event === "Guide"){
-      setPopupContent("Guide")
-    }else{
+  const togglePopup = (event) => {
+    console.log("event...poput...", event);
+    if (event === "Guide") {
+      setPopupContent("Guide");
+    } else {
       setPopupContent("leaderBoard");
     }
     setShowPopup(!showPopup);
@@ -27,14 +29,14 @@ const Header = () => {
     <div className='HeaderContainer'>
       <div className="topElement-container">
         <div className="guide-button">
-          <button onClick={()=>{togglePopup("Guide")}}>
+          <button onClick={() => { togglePopup("Guide") }}>
             <Image src={guideButton} alt="myJumps" />
           </button>
         </div>
         <span>Jumps:xxx</span>
         <Image src={myJumps} alt="myJumps" />
         <div className="leaderboardButton">
-          <button onClick={()=>{togglePopup("leaderBoard")}}>
+          <button onClick={() => { togglePopup("leaderBoard") }}>
             <Image src={leaderBoardButton} alt="myJumps" />
           </button>
         </div>
@@ -43,8 +45,13 @@ const Header = () => {
       {showPopup && <Popup onClose={togglePopup} popupProps={popupContent} />}
 
       <div class="reward-tile-container">
-            <Image src={titlePosition} alt="rewardHistory" />
-            <Image src={rewardHistory} alt="tilepositionIcon" />
+        <Image src={titlePosition} alt="rewardHistory" />
+        <Image src={rewardHistory} alt="tilepositionIcon" />
+      </div>
+
+      <div class="character-container">
+        <Image src={characterLeft} alt="rewardHistory" />
+        <Image src={characterRight} alt="tilepositionIcon" />
       </div>
     </div>
   );
