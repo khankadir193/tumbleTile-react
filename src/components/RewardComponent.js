ecc0fa8ed941604ca09d41e2d496c55c7a09f542
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-05-02 13:15:55 +0530

    daily over all button

--- src/components/RewardComponent.js
+++ src/components/RewardComponent.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import Image from './Image';
 import './Reward.css'
+import hourlySelected from '../images/Leaderboard/Hourly-selected.png';
+import dailySelected from '../images/Leaderboard/Daily-selected.png';
+import overAllSelected from '../images/Leaderboard/Overall-selected.png';
+import hourlyUnSelected from '../images/Leaderboard/Hourly-unselected.png';
+import dailyUnSelected from '../images/Leaderboard/Daily-unselected.png';
+import overAllUnSelected from '../images/Leaderboard/Overall-unselected.png';
+
 
 const RewardComponent = () => {
-  return (
-    <div className='reward-container'>
+    return (
+        <div className='reward-container'>
+            <div className='reward-hourly-daily-overallBtn'>
+                <button>
+                    <Image src={dailySelected} alt="dailySelected" />
+                </button>
 
-    </div>
-  );
+                <button>
+                    <Image src={overAllUnSelected} alt="overAllUnSelected" />
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default RewardComponent
