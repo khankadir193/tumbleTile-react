f326a275df498dbf35c63579cdddae6e2abb9f8c
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-05-05 22:26:10 +0530

    api calling functionality has been done

--- src/LeaderBoard/OverAllComp.js
+++ src/LeaderBoard/OverAllComp.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 const OverAll = () => {
   console.log('overall Leaderboard component');
+  const [leaderboardInfo, setLeaderboardInfo] = useState(null);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://www.streamkar.net/api/activity/eidF/getLeaderboardInfoV2?eventDesc=20240422_tile&rankIndex=12&pageNum=1&pageSize=20');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log("data...???",data);
+        setLeaderboardInfo(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // empty dependency array to run only once on component mount
+
 
   return (
     <div>OverAll</div>
